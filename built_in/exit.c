3c417d0da5a4eeaed855ca7a22197339aa53f75e
Jonas Hermon-duc <jhermon-@student.42lausanne.ch>
2022-04-13 16:18:58 +0200

    exit built_in working

--- built_in/exit.c
+++ built_in/exit.c
@@ -12,7 +12,7 @@ void	check_arg(char	*arg)
 		if (!ft_isdigit(arg[i]))
 		{
 			message = ft_strjoin(arg, ": numeric argument required");
-			err_exit("exit: ", message, 255);
+			err_exit("minishell: exit: ", message, 255);
 		}
 		i++;
 	}
@@ -34,26 +34,18 @@ void	ft_exit(t_token *liste_args)
   t_token	*args;
 
   args = liste_args;
-	printf("exit\n");
   if (args->tab_cmd[1] == NULL)
+	{
+		printf("exit\n");
     exit(0);
+	}
   if (args->tab_cmd[2] != NULL)
 	{
-		printf("minishell: exit :too many arguments\n");
+		printf("exit :too many arguments\n");
 		return ;
 	}
   i = exit_arg(args->tab_cmd[1]);
-	//return (printf(" exit stat = %d\n", i));
+	printf("exit\n");
   exit(i);
 	//return (0);
 }
-
-// int main(int argc, char **argv)
-// {
-//     if (argc > 1)
-//     {
-//         printf("%s\n", argv[1]);
-//         ft_exit(&argv[1]);
-//     }
-//     return (0);
-// }
