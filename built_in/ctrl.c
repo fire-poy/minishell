fd9ab2776b6e6041047854a3545a11e4e3a97a13
Jonas Hermon-duc <jhermon-@student.42lausanne.ch>
2022-04-14 15:49:59 +0200

    ctrlc not working

--- built_in/ctrl.c
+++ built_in/ctrl.c
@@ -2,23 +2,24 @@
 
 void  INThandler(int sig)
 {
-     char  c;
-
-     signal(sig, SIG_IGN);
-     printf("OUCH, did you hit Ctrl-C?\n"
-            "Do you really want to quit? [y/n] ");
-     c = getchar();
-     if (c == 'y' || c == 'Y')
-          exit(0);
-     else
-          signal(SIGINT, INThandler);
-     getchar(); // Get new line character
+	if (sig == SIGINT)
+		write(STDOUT_FILENO, "yo\n", 1);
 }
 
-int  main(void)
-{
-     signal(SIGINT, INThandler);
-     while (1)
-          pause();
-     return 0;
-}
+// int  main(void)
+// {
+//      signal(SIGINT, INThandler);
+// 		 signal(SIGQUIT, INThandler);
+//      while (1)
+//           pause();
+//      return 0;
+// }
+
+    //  2     SIGINT       terminate process    interrupt program
+    //  3     SIGQUIT      create core image    quit program
+
+		// The sig argument specifies which signal was received.  The func procedure allows a user to choose the action upon receipt of a signal.  
+		// To set the default action of the signal to occur as listed above,
+    //  func should be SIG_DFL.  A SIG_DFL resets the default action.  
+		// To ignore the signal, func should be SIG_IGN.  This will cause subsequent instances of the signal to be ignored and pending instances to be
+    //  discarded.  If SIG_IGN is not used, further occurrences of the signal are automatically blocked and func is called.
