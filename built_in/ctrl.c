9e1334a2c61468f99caa9198bf03c71ceb5b7e61
Jonas Hermon-duc <jhermon-@student.42lausanne.ch>
2022-04-29 18:28:17 +0200

    here doc ctr+c quit

--- built_in/ctrl.c
+++ built_in/ctrl.c
@@ -1,7 +1,5 @@
 #include "../minishell.h"
 
-pid_t	g_pid[255];
-
 void	ft_get_pid(int id)
 {
 	if (id != 0)
@@ -42,29 +40,18 @@ void	signal_q(int sig)
 
 void	signal_here(int signal)
 {
-	int a = -1;
 	if (signal == SIGINT)
-		write(0, &a, 1);
+	{
+		g_pid[8] = 6;
+		write(1, "\n", 2);
+		rl_on_new_line();
+		rl_replace_line("", 1); // keep
+		rl_redisplay(); //keep
+	}
 		//ilfaut write meme signaux de ctrl+D
 }
 
 void	signal_h(int signal)
-{
-	struct termios	save;
-
-	tcgetattr(0, &save);
-	save.c_lflag &= ~ECHOCTL;
-	if (signal == 2)
-	{
-		write(1, "\n", 2);
-		rl_on_new_line();
-		rl_replace_line("", 0);
-		rl_redisplay();
-	}
-	tcsetattr(0, TCSANOW, &save);
-}
-
-void	signal_2(int signal)
 {
 	struct termios	save;
 
@@ -93,16 +80,9 @@ void	ft_stop2(char *input)
 {
 		if (input == 0)
 		{
-			write(1, "^D", 2);
-			exit(1);
+			write(1, "\n", 2);
+			rl_on_new_line();
+			rl_replace_line("", 0);
+			//rl_redisplay();
 		}
 }
-
-    //  2     SIGINT       terminate process    interrupt program
-    //  3     SIGQUIT      create core image    quit program
-
-		// The sig argument specifies which signal was received.  The func procedure allows a user to choose the action upon receipt of a signal.  
-		// To set the default action of the signal to occur as listed above,
-    //  func should be SIG_DFL.  A SIG_DFL resets the default action.  
-		// To ignore the signal, func should be SIG_IGN.  This will cause subsequent instances of the signal to be ignored and pending instances to be
-    //  discarded.  If SIG_IGN is not used, further occurrences of the signal are automatically blocked and func is called.
